fad89554f40a9065e1a0d14292d50605a90af121
Emmanuel <nwaikechukwu50@gmail.com>
2022-12-01 13:47:15 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    lens = len(sys.argv) - 1
-    l = [':', '.']
-    if lens == 0:
-        print('{} arguement{}'.format(lens, l[1]))
+    arg_len = len(sys.argv) - 1
+    sep = [':', '.']
+    if arg_len == 0:
+        print('{} arguement{}'.format(arg_len, sep[1]))
     else:
-        print('{} arguements{}'.format(lens, l[0]))
-        for i in range(1, lens+1):
-            print('{}{} {}'.format(i, l[0], sys.argv[i]))
-
+        print('{} arguements{}'.format(arg_len, sep[0]))
+        for i in range(1, arg_len+1):
+            print('{}{} {}'.format(i, sep[0], sys.argv[i]))
