7297836011f6ba892902142a613f2c0ad919bb6f
Emmanuel <nwaikechukwu50@gmail.com>
2022-12-01 22:21:21 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-form sys import argv
+from sys import argv
 if __name__ == "__main__":
     lens = len(argv) - 1
-    sep = [':', '.']
     if lens == 0:
-        print('{} arguements{}'.format(lens, sep[1]))
+        print('{} arguements.'.format(lens))
+    elif lens == 1:
+        print('{} arguement:'.format(lens))
+        print('{}: {}'.format(lens, argv[1]))
     else:
-        if lens == 1:
-            print('{} arguement{}'.format(lens, sep[0]))
-            print('{}{} {}'.format(1, sep[0], argv[1]))
-        else:
-            print('{} arguements{}'.format(lens, sep[0]))
-            for i in range(1, lens+1):
-                print('{}{} {}'.format(i, sep[0], argv[i]))
+        print('{} arguements:'.format(lens))
+        for i in range(1, lens+1):
+            print('{}: {}'.format(i, argv[i]))
