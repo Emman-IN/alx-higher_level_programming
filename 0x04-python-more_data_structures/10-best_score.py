af7d0905f68685759143840d17d0247381412a56
Emmanuel <nwaikechukwu50@gmail.com>
2022-12-07 10:24:36 -0800

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    valueSorted = dict(sorted(a_dictionary.items(),
-                       key=lambda x: x[1]), reverse=True)
-    first = list(valueSorted.keys())[0]
-    return str(first)
+    if a_dictionary:
+        valueSorted = dict(sorted(a_dictionary.items(),
+                           key=lambda x: x[1]), reverse=True)
+        first = list(valueSorted.keys())[0]
+        return str(first)
+    return None
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    new = set()
-    for item in zip(set_1, set_2):
-        if set_1 == set_2:
-            new.add(set_1)
-    return new
+def number_keys(a_dictionary):
+    List = list(a_dictionary.keys())
+    return len(List)
