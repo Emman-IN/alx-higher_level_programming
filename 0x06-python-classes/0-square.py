28419837d5ff15a0d52e60e4f331d4b4e0e81a06
Emmanuel <nwaikechukwu50@gmail.com>
2023-01-06 22:49:18 -0800

    1-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""implementation of the sSquare class"""
+
 
 class Square:
     """class that defines a square"""
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""implemenntation of the Square class"""
+
 
 class Square:
     """class that defines a square"""
     def __init__(self, size):
+        """initialize the square with attributes
+        Args:
+            size (int): The square's size
+            """
+
         self.__size = size
-
-
