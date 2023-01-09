e47e52b3de93953482284c6fb7c3572468987cc8
Emmanuel <nwaikechukwu50@gmail.com>
2023-01-09 02:28:23 -0800

    101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -51,11 +51,10 @@ class Square:
         if self.__size == 0:
             print()
             return
-        else:
-            for i in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                print('{}{}'.format(' '*self.__position[0], '#'*self.__size))
+        for i in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print('{}{}'.format(' '*self.__position[0], '#'*self.__size))
 
     def __str__(self):
         """prints the square"""
